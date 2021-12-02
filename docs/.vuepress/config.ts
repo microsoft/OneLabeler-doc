6b820c7bed214da0d0c5551022c1da893d55ec24
Yu Zhang <yuzhang94@outlook.com>
2021-12-02 14:59:43 +0800

    build: update base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 
 export default defineUserConfig<DefaultThemeOptions>({
-  base: '/onelabeler.github.io/',
+  base: '/OneLabeler.github.io/',
   lang: 'en-US',
   title: 'OneLabeler',
   description: 'Just playing around',
