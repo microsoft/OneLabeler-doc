682ee6331342a7b834d1d8966937e3859513d6c8
Yu Zhang <yuzhang94@outlook.com>
2022-03-11 20:33:28 +0800

    fix(vuepress): update base

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,8 +1,7 @@
 import { defineUserConfig } from 'vuepress'
 import type { DefaultThemeOptions } from 'vuepress'
 
-// const base = '';
-const base = '/OneLabeler.github.io/';
+const base = '/OneLabeler-doc/';
 
 export default defineUserConfig<DefaultThemeOptions>({
   base,
